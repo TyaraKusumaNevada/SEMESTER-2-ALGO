434e972c8b23001a43d5f7c8f27fd164107ed8f3
TyaraKusumaNevada <tyaranevada29@gmail.com>
2024-03-31 00:20:40 +0700

    jobsheet 6 verifikasi percobaan 1

--- Jobsheet6/BukuMain28.java
+++ Jobsheet6/BukuMain28.java
@@ -1,16 +1,16 @@
 package Jobsheet6;
 import java.util.Scanner;
 public class BukuMain28 {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Scanner s1 = new Scanner(System.in);
-        
+
         PencarianBuku28 data = new PencarianBuku28();
         int jumBuku = 5;
-        
+
         System.out.println("-------------------------------------");
         System.out.println("Masukkan data Buku secara urut dari kode buku terkecil : ");
-        for (int i =0; i<jumBuku; i++){
+        for (int i = 0; i < jumBuku; i++) {
             System.out.println("-----------------");
             System.out.print("Kode Buku \t : ");
             int kodeBuku = s.nextInt();
@@ -26,7 +26,7 @@ public class BukuMain28 {
             Buku28 m = new Buku28(kodeBuku, judulBuku, tahunTerbit, pengarang, stok);
             data.tambah(m);
         }
-        
+
         System.out.println("============================================");
         System.out.println("Data keseluruhan Buku");
         data.tampil();
@@ -39,12 +39,5 @@ public class BukuMain28 {
         System.out.println("Menggunakan sequential Search");
         int posisi = data.FindSeqSearch(cari);
         data.Tampilposisi(cari, posisi);
-
     }
-
-   
-    }
-
-
-
-
+}
--- Jobsheet6/PencarianBuku28.java
+++ Jobsheet6/PencarianBuku28.java
@@ -3,30 +3,28 @@ package Jobsheet6;
 import java.util.Scanner;
 
 public class PencarianBuku28 {
-    Buku28 listBk[] = new Buku28 [5];
+    Buku28 listBk[] = new Buku28[5];
     int idx;
 
     void tambah(Buku28 m) {
-        if(idx < listBk.length) {
+        if (idx < listBk.length) {
             listBk[idx] = m;
             idx++;
-        }else {
+        } else {
             System.out.println("Data sudah penuh!");
-
         }
-
     }
 
-    void tampil(){
-        for (Buku28 m :listBk) {
-            m.tampilDataBuku();
+    void tampil() {
+        for (int i = 0; i < idx; i++) {
+            listBk[i].tampilDataBuku();
         }
     }
 
     public int FindSeqSearch(int cari) {
-        int posisi = 2;
-        for (int j = 0; j < listBk.length; j++) {
-            if (listBk[j].kodeBuku==cari){
+        int posisi = -1;
+        for (int j = 0; j < idx; j++) {
+            if (listBk[j].kodeBuku == cari) {
                 posisi = j;
                 break;
             }
@@ -34,12 +32,11 @@ public class PencarianBuku28 {
         return posisi;
     }
 
-    public void Tampilposisi(int x, int pos){
-        if (pos!= -1) {
+    public void Tampilposisi(int x, int pos) {
+        if (pos != -1) {
             System.out.println("data : " + x + " ditemukan pada indeks " + pos);
-        }else {
-            System.out.println("data : "+ x + "Tidak ditemukan");
+        } else {
+            System.out.println("data : " + x + " Tidak ditemukan");
         }
     }
-    
 }
