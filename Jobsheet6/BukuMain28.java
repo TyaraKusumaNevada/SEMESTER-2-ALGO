014259af1f5e707816f8e8080bcfc95722fdaa5e
TyaraKusumaNevada <tyaranevada29@gmail.com>
2024-04-05 13:45:30 +0700

    Latihan Praktikum  no 1

--- Jobsheet6/BukuMain28.java
+++ Jobsheet6/BukuMain28.java
@@ -1,49 +1,56 @@
 package Jobsheet6;
 import java.util.Scanner;
 public class BukuMain28 {
-    public static void main(String[] args) {   
-        Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
         PencarianBuku28 data = new PencarianBuku28();
-        int jumBuku = 5;
+        int jumBuku = 2;
         System.out.println("-------------------------------------");
         System.out.println("Masukkan data Buku secara urut dari kode buku terkecil : ");
-        for (int i = 0; i < jumBuku; i++){
+        for (int i = 0; i < jumBuku; i++) {
             System.out.println("-----------------");
             System.out.print("Kode Buku \t : ");
-            int kodeBuku = s.nextInt();
+            String kodeBuku = s1.nextLine();
             System.out.print("Judul Buku \t : ");
             String judulBuku = s1.nextLine();
             System.out.print("Tahun terbit \t : ");
-            int TahunTerbit = s.nextInt();
+            int tahunTerbit = s1.nextInt();
+            s1.nextLine(); 
             System.out.print("Pengarang \t : ");
             String pengarang = s1.nextLine();
-            System.out.print("Stock \t\t : ");
-            int stok = s.nextInt();
-         
-            Buku28 m = new Buku28(kodeBuku, judulBuku, TahunTerbit, pengarang, stok);
-            data.tambah(m);
+            System.out.print("Stock \t : ");
+            int stock = s1.nextInt();
+            s1.nextLine();  
+
+            Buku28 buku = new Buku28(kodeBuku, judulBuku, tahunTerbit, pengarang, stock);
+            data.tambah(buku);
         }
-        System.out.println("---------------------------------------");
-        System.out.println("Data keseluruhan buku : ");
-        data.tampil();
-        System.out.println("________________________________________");
-        System.out.println("________________________________________");
+
         System.out.println("Pencarian Data : ");
         System.out.println("Masukkan kode buku yang dicari: ");
         System.out.print("Kode buku : ");
-        int cari = s.nextInt();
+        String cari = s1.nextLine();
         System.out.println("================================");
-        System.out.println("menggunakan sequential search ");
+        System.out.println("Menggunakan sequential search ");
         int posisi = data.FindSeqSearch(cari);
         data.Tampilposisi(cari, posisi);
         data.TampilData(cari, posisi);
-        System.out.println("menggunakan metode FindBuku ");
-        Buku28 dataBuku04 = data.FindBuku(cari);
-        dataBuku04.tampilDataBuku();
+
         System.out.println("================================");
-        System.out.println("menggunakan binary search");
-        posisi = data.FindBinarySearch(cari, 0, jumBuku-1);
+        System.out.println("Menggunakan binary search");
+        posisi = data.FindBinarySearch(cari, 0, jumBuku - 1);
+        data.Tampilposisi(cari, posisi);
+        data.TampilData(cari, posisi);
+
+        System.out.println("================================");
+        System.out.println("Menggunakan sequential search untuk String");
+        posisi = data.FindSeqSearchByString(cari);
+        data.Tampilposisi(cari, posisi);
+        data.TampilData(cari, posisi);
+
+        System.out.println("================================");
+        System.out.println("Menggunakan binary search untuk String");
+        posisi = data.FindBinarySearchByString(cari, 0, jumBuku - 1);
         data.Tampilposisi(cari, posisi);
         data.TampilData(cari, posisi);
     }
--- Jobsheet6/PencarianBuku28.java
+++ Jobsheet6/PencarianBuku28.java
@@ -3,68 +3,98 @@ package Jobsheet6;
 import java.util.Scanner;
 
 public class PencarianBuku28 {
-    Buku28 listBk [] = new Buku28 [5];
+    Buku28[] listBK = new Buku28[2];
     int idx;
-    
+
     void tambah(Buku28 m) {
-        if (idx < listBk.length) {
-            listBk[idx] = m;
-            idx ++;
-        }else {
+        if (idx < listBK.length) {
+            listBK[idx] = m;
+            idx++;
+        } else {
             System.out.println("Data sudah penuh!");
         }
     }
-    void tampil () {
-        for (Buku28 m : listBk ) {
+
+    void tampil() {
+        for (Buku28 m : listBK) {
             m.tampilDataBuku();
         }
     }
-    public int FindSeqSearch(int cari ) {
+
+    public int FindSeqSearch(String cari) {
         int posisi = -1;
-        for (int j=0; j< listBk.length; j++) {
-            if (listBk[j].kodeBuku == cari) {
+        for (int j = 0; j < listBK.length; j++) {
+            if (listBK[j].kodeBuku.equals(cari)) {
                 posisi = j;
                 break;
             }
         }
         return posisi;
     }
-    public Buku28 FindBuku(int cari){
-        for (Buku28 buku : listBk) {
-            if (buku.kodeBuku == cari) {
+
+    public Buku28 FindBuku(String cari) {
+        for (Buku28 buku : listBK) {
+            if (buku.kodeBuku.equals(cari)) {
                 return buku;
             }
         }
         return null;
     }
-    public void Tampilposisi(int x, int pos) {
-        if (pos!= -1){
-            System.out.println("data : "  + x +  " ditemukan pada indeks " + pos);
+
+    public void Tampilposisi(String x, int pos) {
+        if (pos != -1) {
+            System.out.println("data : " + x + " ditemukan pada indeks " + pos);
         } else {
-            System.out.println("data " + x + "tidak ditemukan");
+            System.out.println("data " + x + " tidak ditemukan");
         }
     }
-    public void TampilData(int x, int pos){
-        if (pos!= -1){
+
+    public void TampilData(String x, int pos) {
+        if (pos != -1) {
             System.out.println("Kode Buku \t : " + x);
-            System.out.println("Judul     \t : " +listBk[pos].judulBuku);
-            System.out.println("Tahun Terbit \t : " +listBk[pos].tahunTerbit);
-            System.out.println("Pengarang \t : " +listBk[pos].pengarang);
-            System.out.println("Stock     \t : " +listBk[pos].stock);
+            System.out.println("Judul     \t : " + listBK[pos].judulBuku);
+            System.out.println("Tahun Terbit \t : " + listBK[pos].tahunTerbit);
+            System.out.println("Pengarang \t : " + listBK[pos].pengarang);
+            System.out.println("Stock     \t : " + listBK[pos].stock);
         } else {
-            System.out.println("data " + x + "tidak ditemukan");
+            System.out.println("data " + x + " tidak ditemukan");
         }
     }
-    public int FindBinarySearch(int cari, int left, int right){
-        int mid;
-        if (right >= left){
-            mid = left+ (right - left)/2;
-            if (cari == listBk[mid].kodeBuku) {
-                return (mid);
-            } else if (listBk[mid].kodeBuku < cari) {
-                return FindBinarySearch(cari, left, mid-1);
-            }else {
-                return FindBinarySearch(cari, mid + 1, right);
+
+    public int FindBinarySearch(String cari, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (cari.equals(listBK[mid].kodeBuku)) {
+                return mid;
+            } else if (cari.compareTo(listBK[mid].kodeBuku) < 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int FindSeqSearchByString(String cari) {
+        int posisi = -1;
+        for (int j = 0; j < listBK.length; j++) {
+            if (listBK[j].kodeBuku.equals(cari)) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    public int FindBinarySearchByString(String cari, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (cari.equals(listBK[mid].kodeBuku)) {
+                return mid;
+            } else if (cari.compareTo(listBK[mid].kodeBuku) < 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return -1;
